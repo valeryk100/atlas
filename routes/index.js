af3f94eda2c1d6a4aef5ae361db920288381d2be
valerykr <46300247+valeryk100@users.noreply.github.com>
2023-07-20 20:14:00 +0300

    second

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express= require("express");
 const router = express.Router();
 
 router.get("/" , (req,res)=> {
-  res.json({msg:"Rest api work 2222 !"})
+  res.json({msg:"Rest api work 2222 10 !"})
 })
 
 module.exports = router;
